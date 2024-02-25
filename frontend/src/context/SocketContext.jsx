ff90751fcc424a7c30301589e88093714c1c16ee
vikashsinha2603 <vikashsinha045@gmail.com>
2024-02-25 17:52:17 +0530

    lll

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://we-chat-aa2v.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
